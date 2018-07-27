978e286a8eeb8f688332652cc4459c04f6436898
Khyati Gandhi <khyatigandhi1998@gmail.com>
2018-07-27 17:15:11 +0530

    background services enable disable

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -0,0 +1,38 @@
+package com.bignerdranch.android.photogallery;
+
+import android.annotation.SuppressLint;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class PollService extends IntentService {
+
+    private static final String TAG = "PollService";
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PollService.class);
+    }
+    public PollService() {
+        super(TAG);
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+
+        Log.i(TAG, "Received an intent: " + intent);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        @SuppressLint("MissingPermission") boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable &&
+                cm.getActiveNetworkInfo().isConnected();
+        return isNetworkConnected;
+    }
+}
